ee5225acd02b3a80586f35237ff9df6b77930c83
Cayo Andre Alves Zickler <126572285+Cayozickler@users.noreply.github.com>
2024-09-05 10:53:23 -0300

    Update index .js

--- index .js
+++ index .js
@@ -1,6 +1,17 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
+const arrayDataSemana = document.getElementById
+
+const dialogPonto = document.getElementById("dialog-ponto");
+
+const btnRegistrarPonto = document.getElementById("bnt-registrar-ponto");
+bntRegistrarPonto.addEvetlistener("Click",() =>{
+    dialogPonto.ShowModal();
+});
+
+
+dialogPonto.ShowModal();
 
 // todos conjuntos numericos (exeto ano )deve ter 2 digitos (adiconar 0 se for menor que 10)
 // Retornar dia da semana por extenso(em pt-br)
@@ -8,7 +19,7 @@ const horaMinSeg = document.getElementById("hora-min-seg");
 function diaSemana(){
     // retornar dia da diaSemana
     const dia = new dia();
-    return dia.getDia()
+    return arrayDayWeek[date.getDay()];
 }
 
 function dataCompleta(){
@@ -19,5 +30,14 @@ function horaCompleta(){
     const data = new date();
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
+
+function atualzarHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
+
+setInterval(atualzarHora, 1000); 
+
+diaSemana.textContent = diaSemana();
 diaMesAno.textContent = dataCompleta();
-horaMinSeg.textContent = horaCompleta();
+
+
